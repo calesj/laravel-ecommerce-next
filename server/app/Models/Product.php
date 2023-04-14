ab523337e895cc20d25ecbda797a47a8f1d7f28c
Cales <86434489+calesj@users.noreply.github.com>
2023-04-14 17:30:06 -0300

    feat - criando model Product

---
+++ server/app/Models/Product.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'api_id',
+        'origins',
+        'name',
+        'description',
+        'category',
+        'price',
+        'material',
+        'department'
+    ];
+
+    // relacionamento muito pra muitos, onde um produto pode estar presente em varios carrinhos
+    public function carts()
+    {
+        return $this->belongsToMany(Cart::class, 'cart_product');
+    }
+}
