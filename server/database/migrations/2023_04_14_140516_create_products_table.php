97cf2420506bfc09c1a13de7368dee67d64f2efe
Cales <86434489+calesj@users.noreply.github.com>
2023-04-14 17:29:58 -0300

    feat - criando as migrations de produtos, do carrinho, e da tabela pivo

---
+++ server/database/migrations/2023_04_14_140516_create_products_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('api_id');
+            $table->string('origins');
+            $table->string('name');
+            $table->string('description')->nullable();
+            $table->string('category')->nullable();
+            $table->decimal('price');
+            $table->string('material')->nullable();
+            $table->string('department')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
