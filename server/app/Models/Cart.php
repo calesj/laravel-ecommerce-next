94170a11930cb3b762cad18887019ce6ae6c9ca1
Cales <86434489+calesj@users.noreply.github.com>
2023-04-14 10:40:40 -0300

    feat - criando a relacao 1 pra 1 no model Cart, onde um cart pertence a um usuario, e definindo os atributtes com valores vazios

---
+++ server/app/Models/Cart.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Cart extends Model
+{
+    use HasFactory;
+
+    protected $attributes = [
+        'origin' => '',
+        'name'=> '',
+        'description'=> '',
+        'price'=> 0,
+        'category'=> ''
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
