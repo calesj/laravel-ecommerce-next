471fc490f10295dc0e982f1b2559659503d3996e
Cales <86434489+calesj@users.noreply.github.com>
2023-04-14 17:31:11 -0300

    feat - alterando o controller do carrinho para o contexto da nossa aplicacao

--- server/app/Http/Controllers/CartController.php
+++ server/app/Http/Controllers/CartController.php
@@ -5,69 +5,99 @@ namespace App\Http\Controllers;
 use App\Form\FormValidation;
 use App\Http\Controllers\Controller;
 use App\Models\Cart;
+use App\Models\Product;
 use Illuminate\Database\QueryException;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class CartController extends Controller
 {
+    private $rules = [
+        'api_id' => 'required',
+        'origins' => 'required',
+        'name' => 'required',
+        'price' => 'required|numeric',
+        'quantity' => 'required'
+    ];
 
-    public function index()
+    private $request;
+
+    public function __construct(Request $request)
     {
-        $carts = Cart::all();
-
-        if (!$carts) {
-            return response()->json(['message' => 'recurso nao encontrado'], 204);
-        }
-
-        return response()->json($carts);
+        $this->request = $request;
     }
 
-    public function show($id)
+    public function addToCart()
     {
-        $cart = Cart::first($id);
-        if (!$cart) {
-            return response()->json(['message' => 'recurso nao encontardo'], 204);
+        DB::beginTransaction();
+
+        $validate = FormValidation::validar($this->request->all(), $this->rules);
+
+        if ($validate !== true) {
+            return $validate;
         }
+
+        try {
+            // quando o usuario esta autenticado, um token e gerado pra identificar esse usuario,
+            // o cliente salva esse token no localStorage da pagina, de uma forma que toda requisicao que esse usuario
+            // fizer, ela envie esse token como parametro automaticamente de forma escondida
+            // aqui ele esta enviando o token de autenticacao
+            // o proprio laravel identifica o usuario atraves desse token, e faz o trabalho aqui em cima desse usuario
+            $user = $this->request->user();
+
+            // aqui estamos verificando se o produto existe no banco, se nao existir ele cria um novo
+            $product = Product::where('api_id', $this->request->api_id)->first();
+            if (!$product){
+                $product = new Product;
+                $product->name = $this->request->name;
+                $product->api_id = $this->request->api_id;
+                $product->origins = $this->request->origins;
+                $product->price = $this->request->price;
+                $product->save();
+            }
+
+
+            // acessando o carrinho de compras atraves do usuario, depois acessamos o metodo
+            // products do model cart, e adicionamos o produto salvo a ele
+            $user->cart->products()->attach($product, ['quantity' => $this->request->quantity]);
+
+            DB::commit();
+
+            return response()->json(['message' => 'Produto adicionado ao carrinho']);
+
+        } catch (QueryException $e) {
+            DB::rollBack();
+
+            return response()->json($e);
+        }
+
+    }
+
+    public function getProductsCart()
+    {
+        // com o token de acesso, ele verifica qual o usuario esta fazendo a requisicao
+        $user = $this->request->user();
+
+        // atraves desse usuario, ele retorna os produtos que estao no carrinho desse usuario
+        $cart = $user->cart()->with('products')->get();
 
         return response()->json($cart);
     }
 
-    public function store(Request $request)
+    public function removeFromCart($id)
     {
-        try {
-            $cart = new Cart();
-            $cart->name = $request->nome;
-            $cart->description = $request->description;
-            $cart->price = $request->price;
-            $cart->category = $request->category;
-            $cart->save();
-            return response()->json(['message' => 'Oba, deu certo!']);
-        } catch (QueryException $e) {
-            return response()->json(['message' => 'Desculpe, algo deu errado!'], 501);
-        }
-    }
+        $user = $this->request->user();
+        $products = Product::where('api_id', $id)->get();
+        foreach ($products as $product) {
+            try {
 
-    public function update($id, Request $request)
-    {
-        try {
-            $cart = Cart::first($id);
-            if (!$cart) {
-                return response()->json(['message' => 'recurso nao encontado', 204]);
+                $user->cart->products()->detach($product->id);
+
+            } catch (QueryException $e) {
+                return response()->json($e);
             }
-            $cart->name = $request->nome;
-            $cart->save();
-        } catch (QueryException $e) {
-            return response()->json(['message' => 'Desculpe, algo deu errado'], 501);
-        }
-    }
-
-    public function delete($id)
-    {
-        $cart = Cart::first($id);
-        if (!$cart) {
-            return response()->json(['message' => 'recurso nao encontado', 204]);
         }
 
-        $cart->delete();
     }
+
 }
